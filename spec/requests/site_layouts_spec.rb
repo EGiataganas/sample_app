485c59db05161d6ebfd7ed2c290aee49cc2e9bc4
Evangelos Giataganas <e.giataganas@gmail.com>
2015-03-11 12:14:02 +0000

    implement feature test with capybara

--- spec/requests/site_layouts_spec.rb
+++ spec/requests/site_layouts_spec.rb
@@ -1,13 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "SiteLayouts", type: :request do
+RSpec.feature "site links", type: :feature do
 
-  it "should test layout links" do
-    get root_path
-    expect(response.body).to match("a href=\"#{root_path}\"")
-    expect(response.body).to match("a href=\"#{help_path}\"")
-    # expect(response.body).to include 'a href="/help"'
-    expect(response.body).to match("a href=\"#{about_path}\"")
-    expect(response.body).to match("a href=\"#{contact_path}\"")
+  before (:each) do
+    visit root_path
+  end
+
+  describe "should test layout links" do
+    it "returns the Home page at '/'" do
+	    expect(page).to have_link("Home", href: root_path)
+	end
+
+	it "returns the Help page at '/'help" do
+		expect(page).to have_link("Help", href: help_path)
+	end
+
+	it "returns the About page at '/about'" do
+		expect(page).to have_link("About", href: about_path)
+	end
+
+	it "returns the Contact page at '/contact'" do
+		expect(page).to have_link("Contact", href: contact_path)
+	end
   end
 end
