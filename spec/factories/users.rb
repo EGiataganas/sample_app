075a43786318b7c81876326c8908ffeca0df25c1
Evangelos Giataganas <e.giataganas@gmail.com>
2015-03-12 15:05:05 +0000

    Created the user model

---
+++ spec/factories/users.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+FactoryGirl.define do
+  factory :user do
+    name "MyString"
+email "MyString"
+  end
+
+end
