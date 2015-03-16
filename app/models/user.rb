2fb7aee3fb1a9aa4e5b44193b92ac22919705b25
Evangelos Giataganas <e.giataganas@gmail.com>
2015-03-16 18:43:50 +0000

    Finished the password machinery

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,19 @@
 #
 # Table name: users
 #
-#  id                :integer          not null, primary key
-#  name              :string
-#  email             :string
-#  created_at        :datetime         not null
-#  updated_at        :datetime         not null
-#  ecrypted_password :string
+#  id                 :integer          not null, primary key
+#  name               :string
+#  email              :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  encrypted_password :string
+#  salt               :string
 #
 
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_save :encrypt_password
+
   attr_accessor :password
   # attr_accessible :name, :email, :password, :password_confirmation
 
@@ -22,4 +25,34 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
 
   validates :password, presence: true, confirmation: true, length: { in: 6..40 }
+
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  class << self
+    def authenticate(email, submitted_password)
+      user = find_by_email(email)
+      (user && user.has_password?(submitted_password)) ? user : nil
+    end
+  end
+  
+  private
+  
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+  
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
 end
