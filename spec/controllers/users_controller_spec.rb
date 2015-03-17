d83f1873cc9a044ca9292cc2a0cf06071ea0c7f2
Evangelos Giataganas <e.giataganas@gmail.com>
2015-03-17 12:45:12 +0000

    Added factories and tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  render_views
+
+  before (:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  describe "GET #show" do
+
+    before (:each) do
+      @user = FactoryGirl.create :user
+    end
+
+    it "returns http success" do
+      get :show, id: @user
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns the right user" do
+      get :show, id: @user
+      expect(assigns(:user)).to eq(@user)
+    end
+  end
 
   describe "GET #new" do
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
+    end
+
+
+    it "returns the right title" do
+      get :new
+      expect(response.body).to have_content("Sign up | #{@base_title}")
     end
   end
 
