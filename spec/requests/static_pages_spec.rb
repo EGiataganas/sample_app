36705f66a067ada0915af13cd7ba3e2837cfb254
Evangelos Giataganas <e.giataganas@gmail.com>
2015-05-09 17:03:34 +0100

    refactor Home page test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 RSpec.feature "Static Pages", type: :feature do
 	
 	describe "Home Page" do
-    it "should have the content 'Sample App'", focus: true do  
-    	visit root_path  	
+  	before(:each) { visit root_path }
+    it "should have the content 'Sample App'", focus: true do    	
       expect(page).to have_content('Sample App')
     end
     it "should have the right title" do
-    	visit root_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
   end
