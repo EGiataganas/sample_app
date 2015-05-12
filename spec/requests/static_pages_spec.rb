88d8f2b13355be5486a7252741d5f8ca217b5e53
Evangelos Giataganas <e.giataganas@gmail.com>
2015-05-12 22:13:29 +0100

    refactor statc pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,22 +1,18 @@
 require 'rails_helper'
-# include ApplicationHelper
 
 RSpec.feature 'Static Pages', type: :feature do
 
   subject { page }
-
-  shared_examples_for 'all static pages' do
-    it { should have_selector('h1', text: heading) }
-    it { should have_title(full_title(page_title)) }
-  end
 
   describe 'Home Page' do
   	before(:each) { visit root_path }
     let(:heading)    { 'Sample App' }
     let(:page_title) { '' }
     
-    it_should_behave_like 'all static pages'
-    it { should_not have_title('Home | ' + full_title) }
+    it_behaves_like 'all static pages'
+    it_behaves_like 'a page with site links'
+    it { is_expected.not_to have_title('Home | ' + full_title) }
+    it { is_expected.to have_link('Sign up', href: signup_path)}
   end
 
   describe 'Contact Page' do
@@ -24,7 +20,8 @@ RSpec.feature 'Static Pages', type: :feature do
     let(:heading)    { 'Contact' }
     let(:page_title) { 'Contact' }
     
-    it_should_behave_like 'all static pages'
+    it_behaves_like 'all static pages'
+    it_behaves_like 'a page with site links'
   end
 
   describe 'About Us Page' do
@@ -32,7 +29,8 @@ RSpec.feature 'Static Pages', type: :feature do
     let(:heading)    { 'About Us' }
     let(:page_title) { 'About Us' }
     
-    it_should_behave_like "all static pages"
+    it_behaves_like "all static pages"
+    it_behaves_like 'a page with site links'
   end
 
   describe 'Help Page' do
@@ -40,28 +38,7 @@ RSpec.feature 'Static Pages', type: :feature do
     let(:heading)    { 'Help' }
     let(:page_title) { 'Help' }
     
-    it_should_behave_like 'all static pages'
-  end
-
-  it 'should have the right site layout links' do
-
-    visit root_path
-    click_link 'Sign up'
-    expect(page.current_path).to eq(signup_path)
-
-    click_link 'Home'
-    expect(page).to have_link('Home', href: root_path)
-
-    click_link 'Help'
-    expect(page).to have_link('Help', href: help_path)
-
-    click_link 'About'
-    expect(page).to have_link('About', href: about_path)
-
-    click_link 'Contact'
-    expect(page.current_path).to eq(contact_path)
-
-    click_link 'Log in'
-    expect(page.current_path).to eq(login_path)
+    it_behaves_like 'all static pages'
+    it_behaves_like 'a page with site links'
   end
 end
