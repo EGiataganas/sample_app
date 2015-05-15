6ebebcd4b190bf71779abadd410c84f239e15899
Evangelos Giataganas <e.giataganas@gmail.com>
2015-05-15 13:16:39 +0100

    try to create separate feature test for sign up

--- spec/requests/users_signups_spec.rb
+++ spec/requests/users_signups_spec.rb
@@ -1,32 +1,86 @@
 require 'rails_helper'
 
 RSpec.feature "UsersSignups", :type => :feature do
+  background { visit signup_path }
+  given(:submit) { "Create my account" }
+  
   scenario "invalid signup information" do
-    visit signup_path
-
     fill_in "Name", :with => ""
     fill_in "Email", :with => "user@invalid"
     fill_in "Password", :with => "foo"
     fill_in "Confirmation", :with => "bar"
-    click_button "Create my account"
 
-    expect(User.count).to be_zero
+    expect { click_button submit }.not_to change(User, :count)
     expect(page).to have_text("The form contains 5 errors.")
-    # expect(page.current_path).to eq new_user_path
+    expect(current_path).not_to eq new_user_path
   end
 
   scenario "valid signup information" do
-    visit signup_path
-
     fill_in "Name", :with => "User Example"
     fill_in "Email", :with => "user@valid.com"
     fill_in "Password", :with => "foobar"
     fill_in "Confirmation", :with => "foobar"
-    click_button "Create my account"
-
-    expect(User.count).to be(1)
+    
+    expect { click_button submit }.to change(User, :count)
     expect(page).to have_text("Welcome to the Sample App!")
-    # expect(page.current_path).to eq(user_path, id: 1)
-    # expect(page).to be_true is_logged_in?
+
+
+    # given(:user) { User.find_by(email: 'user@example.com') }
+
+    # it { should have_title(user.name) }
+    # expect(User.find_by(email: 'user@valid.com')).to be_true(is_logged_in?)
+    # expect(page.current_path).not_to eq new_user_path
+    # expect(page).to have_text("Welcome to the Sample App!")
   end
+
+  # describe "sign up" do
+    # background { visit signup_path }
+
+    # given(:submit) { "Create my account" }
+
+    # scenario "with invalid information" do
+    #   # it "should not create a user" do
+    #     expect { click_button submit }.not_to change(User, :count)
+    #   # end
+    # end
+
+    # # describe "with valid information" do
+    #   background do
+    #     fill_in "Name",         with: "Example User"
+    #     fill_in "Email",        with: "user@example.com"
+    #     fill_in "Password",     with: "foobar"
+    #     fill_in "Confirmation", with: "foobar"
+    #   end
+
+    #   scenario "should create a user" do
+    #     expect { click_button submit }.to change(User, :count).by(1)
+    #   end
+  #   end
+  # end
+
+  # describe "sign up" do
+  #   before { visit signup_path }
+
+  #   let(:submit) { "Create my account" }
+
+  #   describe "with invalid information" do
+  #     it "should not create a user" do
+  #       expect { click_button submit }.not_to change(User, :count)
+  #     end
+  #   end
+
+  #   describe "with valid information" do
+  #     before do
+  #       fill_in "Name",         with: "Example User"
+  #       fill_in "Email",        with: "user@example.com"
+  #       fill_in "Password",     with: "foobar"
+  #       fill_in "Confirmation", with: "foobar"
+  #     end
+
+  #     it "should create a user" do
+  #       expect { click_button submit }.to change(User, :count).by(1)
+  #     end
+  #   end
+  # end
+
 end
