5bef26efaa4d8e6267e91c4038aab46135d1bb13
Evangelos Giataganas <e.giataganas@gmail.com>
2015-07-01 15:26:21 +0100

    test for login success and unsuccess

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -10,4 +10,29 @@ RSpec.feature 'Authentication', type: :feature do
     it { should have_content('Log in') }
     it { should have_title('Log in') }
   end
+
+  describe "signin" do
+    before { visit login_path }
+
+    describe "with invalid information" do
+      before { click_button "Log in" }
+
+      it { should have_title('Log in') }
+      it { should have_selector('div.alert.alert-danger') }
+    end
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Log in"
+      end
+
+      it { should have_title(user.name) }
+      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Log out',    href: logout_path) }
+      it { should_not have_link('Log in', href: login_path) }
+    end
+  end
 end
